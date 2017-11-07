5a150c15187c09ebec0223ce268f48943eade7d0
bfh <1170126481@qq.com>
2017-11-07 09:03:45 +0800

    测试：跳转到首页

--- src/main/java/com/bfh/controller/TestController.java
+++ src/main/java/com/bfh/controller/TestController.java
@@ -1,18 +1,19 @@
 package com.bfh.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author bfh
  * @Time 2017/11/6
+ * 项目构建前期测试所用
  */
 
-@RestController
+@Controller
 public class TestController {
 
 	@RequestMapping({"/","/index"})
 	public String index() {
-		return "首页测试";
+		return "index";
 	}
 }
