f2c87073bb70026cfb105414a84c9595b0b830cd
bfh <1170126481@qq.com>
2017-11-08 09:13:27 +0800

    添加歌曲实体类

---
+++ src/main/java/com/bfh/vo/Song.java
@@ -0,0 +1,46 @@
+package com.bfh.vo;
+
+/**
+ * @Author bfh
+ * @Time 2017/11/7
+ */
+
+public class Song {
+
+	private String title;//歌曲名
+	private String artist;//歌手
+	private String mp3;//音乐路径
+	private String poster;//封面
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getMp3() {
+		return mp3;
+	}
+
+	public void setMp3(String mp3) {
+		this.mp3 = mp3;
+	}
+
+	public String getPoster() {
+		return poster;
+	}
+
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+}
