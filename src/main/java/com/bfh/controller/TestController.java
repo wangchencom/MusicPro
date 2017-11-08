fceb2d67647972a0d42cc4e0d823f5500280ddbc
bfh <1170126481@qq.com>
2017-11-08 09:14:30 +0800

    测试：首页加载自动获取歌曲列表

--- src/main/java/com/bfh/controller/TestController.java
+++ src/main/java/com/bfh/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.bfh.controller;
 
+import com.bfh.vo.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @Author bfh
@@ -11,6 +16,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
+
+	@RequestMapping("/getSongs")
+	public @ResponseBody List<Song> getSongs() {
+
+
+		Song song = new Song();
+
+		song.setTitle("China-P");
+		song.setArtist("徐梦圆");
+		song.setMp3("music/徐梦圆 - China-P.mp3");
+		song.setPoster("images/m0.jpg");
+
+		Song song1 = new Song();
+
+		song1.setTitle("If I were a Bird");
+		song1.setArtist("黒石ひとみ");
+		song1.setMp3("music/黒石ひとみ - If I were a Bird.mp3");
+		song1.setPoster("images/m0.jpg");
+
+		List<Song> list = new LinkedList<>();
+		list.add(song);
+		list.add(0, song1);//添加元素到栈顶
+
+		return list;
+	}
+
 
 	@RequestMapping({"/","/index"})
 	public String index() {
--- src/main/resources/static/js/jPlayer/demo.js
+++ src/main/resources/static/js/jPlayer/demo.js
@@ -1,94 +1,100 @@
+var songsList = null;
+var playlistOptions = null;
+var myPlaylist = null;
+var json = null;
+
 $(document).ready(function(){
 
-
   //用ajax从服务器获取songsList
-  //springmvc返回的要是json格式，而不是String格式
-
-  var songsList = [
-    {
-      title:"China-P",
-      artist:"徐梦圆",
-      mp3:"music/徐梦圆 - China-P.mp3",
-      poster: "images/m0.jpg"
-    }
-  ];
-
-  var playlistOptions = {
-      enableRemoveControls: true,
-      autoPlay: false//自动播放
-    };
+  // songsList = [
+  //   {
+  //     title:"China-P",
+  //     artist:"徐梦圆",
+  //     mp3:"music/徐梦圆 - China-P.mp3",
+  //     poster: "images/m0.jpg"
+  //   }
+  // ];
 
 
-  //初始化播放器
-  var myPlaylist = new jPlayerPlaylist({
-    jPlayer: "#jplayer_N",
-    cssSelectorAncestor: "#jp_container_N"
-  }, 
-  songsList, 
-  {
-    playlistOptions: playlistOptions,
-    swfPath: "js/jPlayer",
-    supplied: "webmv, ogv, m4v, oga, mp3",
-    smoothPlayBar: true,
-    keyEnabled: true,
-    audioFullScreen: false
-  });
+    //获取歌曲列表并初始化播放器
+    $.get("/getSongs", function(data){
+
+        if (data.length === 0) {//默认歌曲列表
+            songsList = [
+              {
+                title:"China-P",
+                artist:"徐梦圆",
+                mp3:"music/徐梦圆 - China-P.mp3",
+                poster: "images/m0.jpg"
+              }
+            ];
+            playlistOptions = {
+                enableRemoveControls: true,
+                autoPlay: false//自动播放
+            };
+        } else {
+            json = JSON.stringify(data);
+            songsList = JSON.parse(json);
+            playlistOptions = {
+                enableRemoveControls: true,
+                autoPlay: true//自动播放
+            };
+        }
 
 
-  
-  $(document).on($.jPlayer.event.pause, myPlaylist.cssSelector.jPlayer,  function(){
-    $('.musicbar').removeClass('animate');
-    $('.jp-play-me').removeClass('active');
-    $('.jp-play-me').parent('li').removeClass('active');
-  });
+        initJPlayer(songsList, playlistOptions);
 
-  $(document).on($.jPlayer.event.play, myPlaylist.cssSelector.jPlayer,  function(){
-    $('.musicbar').addClass('animate');
-  });
+    });
 
-  $(document).on('click', '.jp-play-me', function(e){
-    e && e.preventDefault();
-    var $this = $(e.target);
-    if (!$this.is('a')) $this = $this.closest('a');
-
-    $('.jp-play-me').not($this).removeClass('active');
-    $('.jp-play-me').parent('li').not($this.parent('li')).removeClass('active');
-
-    $this.toggleClass('active');
-    $this.parent('li').toggleClass('active');
-    if( !$this.hasClass('active') ){
-      myPlaylist.pause();
-    }else{
-      var i = Math.floor(Math.random() * (1 + 7 - 1));
-      myPlaylist.play(i);
-    }
-    
-  });
-
-
-
-  // video
-
-  // $("#jplayer_1").jPlayer({
-  //   ready: function () {
-  //     $(this).jPlayer("setMedia", {
-  //       title: "这是什么",
-  //       m4v: "E:/浏览器下载/web/五月天-好好.mp3",
-  //       ogv: "E:/浏览器下载/web/五月天-好好.mp3",
-  //       webmv: "E:/浏览器下载/web/五月天-好好.mp3",
-  //       poster: "images/m41.jpg"
-  //     });
-  //   },
-  //   swfPath: "js",
-  //   supplied: "webmv, ogv, m4v",
-  //   size: {
-  //     width: "100%",
-  //     height: "auto",
-  //     cssClass: "jp-video-360p"
-  //   },
-  //   globalVolume: true,
-  //   smoothPlayBar: true,
-  //   keyEnabled: true
-  // });
 
 });
+
+
+
+//初始化播放器
+function initJPlayer(songsList, playlistOptions) {
+
+    myPlaylist = new jPlayerPlaylist({
+            jPlayer: "#jplayer_N",
+            cssSelectorAncestor: "#jp_container_N"
+        },
+        songsList,
+        {
+            playlistOptions: playlistOptions,
+            swfPath: "js/jPlayer",
+            supplied: "webmv, ogv, m4v, oga, mp3",
+            smoothPlayBar: true,
+            keyEnabled: true,
+            audioFullScreen: false
+        });
+
+    //初始化按钮时间按钮事件
+    $(document).on($.jPlayer.event.pause, myPlaylist.cssSelector.jPlayer,  function(){
+        $('.musicbar').removeClass('animate');
+        $('.jp-play-me').removeClass('active');
+        $('.jp-play-me').parent('li').removeClass('active');
+    });
+
+    $(document).on($.jPlayer.event.play, myPlaylist.cssSelector.jPlayer,  function(){
+        $('.musicbar').addClass('animate');
+    });
+
+    $(document).on('click', '.jp-play-me', function(e){
+        e && e.preventDefault();
+        var $this = $(e.target);
+        if (!$this.is('a')) $this = $this.closest('a');
+
+        $('.jp-play-me').not($this).removeClass('active');
+        $('.jp-play-me').parent('li').not($this.parent('li')).removeClass('active');
+
+        $this.toggleClass('active');
+        $this.parent('li').toggleClass('active');
+        if( !$this.hasClass('active') ){
+            myPlaylist.pause();
+        }else{
+            var i = Math.floor(Math.random() * (1 + 7 - 1));
+            myPlaylist.play(i);
+        }
+
+    });
+}
