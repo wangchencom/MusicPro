8d49dc961b7b97f1eeae99aa8a31fdda63bc8822
bfh <1170126481@qq.com>
2017-11-10 10:32:37 +0800

    添加mapper注解扫描

--- src/main/java/com/bfh/DemoApplication.java
+++ src/main/java/com/bfh/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.bfh;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.bfh.mapper")
 public class DemoApplication {
 
 	public static void main(String[] args) {
