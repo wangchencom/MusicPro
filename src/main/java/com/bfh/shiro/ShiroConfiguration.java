78ad8f8f2a74bbf2e019bfff645aed1db6b2a415
bfh <1170126481@qq.com>
2017-11-15 08:13:37 +0800

    添加拦截器过滤链

--- src/main/java/com/bfh/shiro/ShiroConfiguration.java
+++ src/main/java/com/bfh/shiro/ShiroConfiguration.java
@@ -66,24 +66,29 @@ public class ShiroConfiguration {
 		Map<String,String> filterChainDefinitionMap = new LinkedHashMap<>();
 
 
-//		filterChainDefinitionMap.put("/getSongs", "anon");
-//		filterChainDefinitionMap.put("/css/**", "anon");
-//		filterChainDefinitionMap.put("/fonts/**", "anon");
-//		filterChainDefinitionMap.put("/images/**", "anon");
-//		filterChainDefinitionMap.put("/js/**", "anon");
-//		filterChainDefinitionMap.put("/music/**", "anon");
-//
-//		filterChainDefinitionMap.put("/", "anon");
-//		filterChainDefinitionMap.put("/index", "anon");
-//		filterChainDefinitionMap.put("/user/login", "anon");
-//		filterChainDefinitionMap.put("/user/register", "anon");
-//		filterChainDefinitionMap.put("/user/logout", "anon");
+
+		filterChainDefinitionMap.put("/css/**", "anon");
+		filterChainDefinitionMap.put("/fonts/**", "anon");
+		filterChainDefinitionMap.put("/images/**", "anon");
+		filterChainDefinitionMap.put("/js/**", "anon");
+		filterChainDefinitionMap.put("/upload/**", "anon");
+
+		filterChainDefinitionMap.put("/", "anon");
+		filterChainDefinitionMap.put("/index", "anon");
+		filterChainDefinitionMap.put("/user/login", "anon");
+		filterChainDefinitionMap.put("/user/register", "anon");
+
+		filterChainDefinitionMap.put("/getSongs", "anon");
+		filterChainDefinitionMap.put("/music/playMusic", "anon");
+		filterChainDefinitionMap.put("/music/musicInfo/**", "anon");
+		filterChainDefinitionMap.put("/music", "anon");
+		filterChainDefinitionMap.put("/main", "anon");
 
 
 
 		//过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 :这是一个坑呢，一不小心代码就不好使了;
 		//authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问
-		filterChainDefinitionMap.put("/**", "anon");
+		filterChainDefinitionMap.put("/**", "authc");
 
 		shiroFilter.setFilterChainDefinitionMap(filterChainDefinitionMap);
 
