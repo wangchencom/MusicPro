c9697d1455a7beebecd7aa1c86ff0715a1c2ac7d
bfh <1170126481@qq.com>
2017-11-17 08:42:39 +0800

    新增：常量工具类

---
+++ src/main/java/com/bfh/utils/ConstUtil.java
@@ -0,0 +1,27 @@
+package com.bfh.utils;
+
+/**
+ * @Author bfh
+ * @Time 2017/11/16
+ * @Description
+ * 积分增加：
+ * 		上传歌曲：+10
+ *		下载歌曲：-5
+ *		用户评论：+2
+ *	等级设置：
+ *		lv1：100
+ */
+public class ConstUtil {
+
+	public static int UPLOAD_MUSIC_SCORE = 10;
+
+	public static int DOWNLOADMUSIC_SCORE = 5;
+
+
+	public static int LV1 = 200;
+	public static int LV2 = 600;
+	public static int LV3 = 1500;
+	public static int LV4 = 3000;
+	public static int LV5 = 5000;
+
+}
